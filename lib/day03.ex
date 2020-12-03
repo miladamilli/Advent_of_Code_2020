9c245d7d3cb172d538f72586536882bb851fceeb
Milada Milli <milada.majak@gmail.com>
2020-12-03 14:31:56 +0100

    Refactoring

--- lib/day03.ex
+++ lib/day03.ex
@@ -15,38 +15,31 @@ defmodule Day03 do
   end
 
   def count_trees(input, slope_right, slope_down) do
-    max_x = Map.keys(input) |> Enum.map(fn {x, _y} -> x end) |> Enum.max()
-    max_y = Map.keys(input) |> Enum.map(fn {_x, y} -> y end) |> Enum.max()
-    run(input, {0, 0}, max_x, max_y, slope_right, slope_down, 0)
+    max_x = Enum.map(input, fn {{x, _y}, _value} -> x end) |> Enum.max()
+    max_y = Enum.map(input, fn {{_x, y}, _value} -> y end) |> Enum.max()
+    run(input, {0, 0}, max_x + 1, max_y, slope_right, slope_down, 0)
   end
 
-  def run(_forest, {_x, max_y}, _max_x, max_y, _slope_right, _slope_down, acc) do
+  def run(_forest, {_x, y}, _max_x, max_y, _slope_right, _slope_down, acc) when y > max_y do
     acc
   end
 
   def run(forest, {x, y}, max_x, max_y, slope_right, slope_down, acc) do
-    position = check_tree({x + slope_right, y + slope_down}, forest)
-
-    if position == nil do
-      run(forest, {x - (max_x + 1), y}, max_x, max_y, slope_right, slope_down, acc)
-    else
-      run(
-        forest,
-        {x + slope_right, y + slope_down},
-        max_x,
-        max_y,
-        slope_right,
-        slope_down,
-        acc + position
-      )
-    end
+    run(
+      forest,
+      {rem(x + slope_right, max_x), y + slope_down},
+      max_x,
+      max_y,
+      slope_right,
+      slope_down,
+      acc + check_tree({x, y}, forest)
+    )
   end
 
   defp check_tree({x, y}, forest) do
     case forest[{x, y}] do
-      "tree" -> 1
-      "empty" -> 0
-      nil -> nil
+      :tree -> 1
+      :empty -> 0
     end
   end
 
@@ -54,17 +47,12 @@ defmodule Day03 do
     file
     |> String.split("\n", trim: true)
     |> Enum.with_index()
-    |> Enum.map(fn {line, index} ->
-      line =
-        String.codepoints(line)
-        |> Enum.map(fn char ->
-          if char == "#", do: "tree", else: "empty"
-        end)
-        |> Enum.with_index()
-
-      Enum.map(line, fn {field, index_field} -> {{index_field, index}, field} end)
+    |> Enum.flat_map(fn {line, y} ->
+      line
+      |> String.codepoints()
+      |> Enum.with_index()
+      |> Enum.map(fn {char, x} -> if char == "#", do: {{x, y}, :tree}, else: {{x, y}, :empty} end)
     end)
-    |> List.flatten()
-    |> Enum.into(%{})
+    |> Map.new()
   end
 end
