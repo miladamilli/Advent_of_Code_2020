d6e9ce14a2e709be341289fdc7656c79292a94e5
Milada Milli <milada.majak@gmail.com>
2020-12-14 13:22:40 +0100

    Day 14 Part 2

--- lib/day14.ex
+++ lib/day14.ex
@@ -57,4 +57,94 @@ defmodule Day14 do
     Enum.zip(value, mask)
     |> Enum.map(fn {val, mask} -> if mask == :x, do: val, else: mask end)
   end
+
+  # PART 2 =============================================
+
+  def part2 do
+    "input/day14.txt"
+    |> File.read!()
+    |> String.split("\n", trim: true)
+    |> run2(nil, %{})
+    |> Map.values()
+    |> Enum.sum()
+  end
+
+  def run2([], _mask, memory) do
+    memory
+  end
+
+  def run2([line | rest], mask, memory) do
+    case parse_line2(line) do
+      {address, value} ->
+        run2(rest, mask, update_memory2(mask, address, value, memory))
+
+      mask ->
+        run2(rest, mask, memory)
+    end
+  end
+
+  defp update_memory2(mask, address, value, memory) do
+    addresses = apply_mask_on_address(mask, address)
+
+    Enum.reduce(addresses, memory, fn address, mem ->
+      Map.put(mem, address, value)
+    end)
+  end
+
+  defp apply_mask_on_address(mask, address) do
+    Enum.zip(address, mask)
+    |> Enum.map(fn {val, mask} ->
+      case mask do
+        0 -> val
+        1 -> 1
+        :floating -> :floating
+      end
+    end)
+    |> generate_address_variants([[]])
+  end
+
+  defp generate_address_variants([], acc) do
+    Enum.map(acc, &Enum.reverse/1)
+    |> Enum.map(&Integer.undigits(&1, 2))
+  end
+
+  defp generate_address_variants([char | rest], acc) do
+    acc =
+      if char != :floating do
+        Enum.map(acc, fn address -> [char | address] end)
+      else
+        Enum.map(acc, fn address -> [1 | address] end) ++
+          Enum.map(acc, fn address ->
+            [0 | address]
+          end)
+      end
+
+    generate_address_variants(rest, acc)
+  end
+
+  defp parse_line2("mask = " <> mask) do
+    mask
+    |> String.codepoints()
+    |> Enum.map(fn
+      "0" -> 0
+      "1" -> 1
+      "X" -> :floating
+    end)
+  end
+
+  defp parse_line2("mem" <> line) do
+    [memory_address, value] = String.split(line, " = ")
+    address = memory_address |> String.trim_leading("[") |> String.trim_trailing("]")
+
+    address =
+      address
+      |> String.to_integer()
+      |> Integer.digits(2)
+      |> Enum.join()
+      |> String.pad_leading(36, "0")
+      |> String.codepoints()
+      |> Enum.map(&String.to_integer/1)
+
+    {address, String.to_integer(value)}
+  end
 end
--- test/day14_test.exs
+++ test/day14_test.exs
@@ -4,4 +4,8 @@ defmodule Day14Test do
   test "part 1" do
     assert Day14.part1() == 7_997_531_787_333
   end
+
+  test "part 2" do
+    assert Day14.part2() == 3_564_822_193_820
+  end
 end
