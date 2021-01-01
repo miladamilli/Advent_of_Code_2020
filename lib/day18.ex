682294e89cd924434597cc9e62d10edcd405fe58
Milada Milli <milada.majak@gmail.com>
2021-01-01 17:07:14 +0100

    Day 18 without Code.eval_string

--- lib/day18.ex
+++ lib/day18.ex
@@ -3,52 +3,101 @@ defmodule Day18 do
     "input/day18.txt"
     |> File.read!()
     |> String.split("\n", trim: true)
-    |> run
-  end
-
-  defp run(data) do
-    data
     |> Enum.map(&eval_line/1)
     |> Enum.map(&String.to_integer/1)
     |> Enum.sum()
   end
 
   defp eval_line(line) do
-    new_line =
-      line
-      |> eval_brackets
-      |> eval_expressions
-
-    if line != new_line, do: eval_line(new_line), else: line
-  end
-
-  defp eval_expressions(line) do
-    line
-    |> String.replace(
-      ~r/\d+\s[\*\+]\s\d+/,
-      fn expression ->
-        {result, _} = Code.eval_string(expression)
-        Integer.to_string(result)
-      end,
-      global: false
-    )
+    case eval_brackets(line) do
+      ^line -> eval_two_numbers(line)
+      other -> eval_line(other)
+    end
   end
 
   defp eval_brackets(line) do
-    new_line =
-      line
-      |> String.replace(
-        ~r/\(\d+\s[\*\+]\s\d+\)/,
+    String.replace(line, ~r/\([^()]+\)/, fn expression ->
+      expression
+      |> String.replace(~r/[()]/, "")
+      |> eval_two_numbers()
+    end)
+  end
+
+  defp eval_two_numbers(expression) do
+    new =
+      String.replace(
+        expression,
+        ~r/\d+ [+*] \d+/,
         fn expression ->
-          {result, _} = Code.eval_string(expression)
-          Integer.to_string(result)
-        end
+          case String.split(expression, [" "]) do
+            [a, "+", b] -> to_string(String.to_integer(a) + String.to_integer(b))
+            [a, "*", b] -> to_string(String.to_integer(a) * String.to_integer(b))
+          end
+        end,
+        global: false
       )
 
-    if new_line != line do
-      eval_brackets(new_line)
-    else
-      line
+    case new do
+      ^expression -> expression
+      other -> eval_two_numbers(other)
+    end
+  end
+
+  # PART 2 =====================
+
+  def part2 do
+    "input/day18.txt"
+    |> File.read!()
+    |> String.split("\n", trim: true)
+    |> Enum.map(&eval_line2/1)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.sum()
+  end
+
+  def eval_line2(line) do
+    case eval_brackets2(line) do
+      ^line -> eval_two_numbers2(line)
+      other -> eval_line2(other)
+    end
+  end
+
+  defp eval_brackets2(line) do
+    String.replace(line, ~r/\([^()]+\)/, fn expression ->
+      expression
+      |> String.replace(~r/[()]/, "")
+      |> eval_two_numbers2()
+    end)
+  end
+
+  defp eval_two_numbers2(expression) do
+    case eval_addition(expression) do
+      ^expression -> eval_multiplication(expression)
+      other -> eval_two_numbers2(other)
+    end
+  end
+
+  defp eval_addition(expression) do
+    String.replace(expression, ~r/\d+ \+ \d+/, fn expression ->
+      [a, b] = String.split(expression, [" + "])
+      to_string(String.to_integer(a) + String.to_integer(b))
+    end)
+  end
+
+  defp eval_multiplication(expression) do
+    new =
+      String.replace(
+        expression,
+        ~r/\d+ \* \d+/,
+        fn expression ->
+          [a, b] = String.split(expression, [" * "])
+          to_string(String.to_integer(a) * String.to_integer(b))
+        end,
+        global: false
+      )
+
+    case new do
+      ^expression -> expression
+      other -> eval_two_numbers(other)
     end
   end
 end
--- test/day18_test.exs
+++ test/day18_test.exs
@@ -6,12 +6,12 @@ defmodule Day18Test do
   end
 
   test "test 2" do
-    assert Day18.run2(["1 + 2 * 3 + 4 * 5 + 6"]) == 231
-    assert Day18.run2(["1 + (2 * 3) + (4 * (5 + 6))"]) == 51
-    assert Day18.run2(["2 * 3 + (4 * 5)"]) == 46
-    assert Day18.run2(["5 + (8 * 3 + 9 + 3 * 4 * 3)"]) == 1445
-    assert Day18.run2(["5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"]) == 669_060
-    assert Day18.run2(["((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"]) == 23340
+    assert Day18.eval_line2("1 + 2 * 3 + 4 * 5 + 6") == "231"
+    assert Day18.eval_line2("1 + (2 * 3) + (4 * (5 + 6))") == "51"
+    assert Day18.eval_line2("2 * 3 + (4 * 5)") == "46"
+    assert Day18.eval_line2("5 + (8 * 3 + 9 + 3 * 4 * 3)") == "1445"
+    assert Day18.eval_line2("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))") == "669060"
+    assert Day18.eval_line2("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2") == "23340"
   end
 
   test "part 2" do
